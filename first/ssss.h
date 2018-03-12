1c703fc20fee0bb35c3d733691c3440eda788c6a
elab <elab@elabs-macbook-pro.local>
2018-03-12 15:06:30 +0800

    33

---
+++ first/ssss.h
@@ -0,0 +1,13 @@
+//
+//  ssss.h
+//  first
+//
+//  Created by elab on 2018/3/12.
+//  Copyright © 2018年 chensy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ssss : NSObject
+
+@end
---
+++ first/ssss.m
@@ -0,0 +1,13 @@
+//
+//  ssss.m
+//  first
+//
+//  Created by elab on 2018/3/12.
+//  Copyright © 2018年 chensy. All rights reserved.
+//
+
+#import "ssss.h"
+
+@implementation ssss
+
+@end
